5388a55b32322d8b35d3265eaa98c9e2b4740573
Akhilesh21 <Akhilesh.mc4@gmail.com>
2020-03-04 16:34:33 +0530

    [Akhilesh]Refactor. refactoring added useStyles  dashbord file

--- src/components/keepBoard.jsx
+++ src/components/keepBoard.jsx
@@ -7,3 +7,13 @@ import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+
+const useStyles = makeStyles({
+    list: {
+      width: 250,
+    },
+    fullList: {
+      width: 'auto',
+    },
+  });
+  

845ba2e1e1be63eb7a3fe81fa5cd841bcd3da348
Akhilesh21 <Akhilesh.mc4@gmail.com>
2020-03-13 18:37:04 +0530

    [Akhilesh]Refactor. addded class coponent for note menu

--- src/components/NoteMenu.jsx
+++ src/components/NoteMenu.jsx
@@ -1,9 +1,25 @@
 import React, { Component } from "react";
-import { PureComponent } from "react";
+//import { PureComponent } from "react";
+
+
 
 
 class NotesMenu extends Component{
     constructor(props){
-        
+        super(props);
+        this.state = {
+             obj:this.props.obj,
+             getNote: this.props.getNote,
+             pinnedStatus: false     
+        };
+    }
+    componentWillReceiveProps(props){
+        this.setState({
+            obj:props.obj
+        });
+    }
+    
+    render() {
+        var pinflag = false;
     }
 } 

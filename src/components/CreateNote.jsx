058e2b500ae73540a8a65619e5bb8e9c0464a931
Akhilesh21 <Akhilesh.mc4@gmail.com>
2020-03-16 09:37:00 +0530

    [Akhilesh]Refactor. modified create note class component

--- src/components/CreateNote.jsx
+++ src/components/CreateNote.jsx
@@ -1,44 +1,14 @@
-import React, { useState ,Fragment,Component, PureComponent } from "react";
-import Card from "@material-ui/core/Card";
+import React, { Component } from 'react'
 
-
-const drawerWidth = 244;
-
-const saveclose = "Save & Close";
-let array = [];
-class CreateNote extends PureComponent{
-    constructor(props){
-        super(props);
-        this.state = {
-            
-            open : false,
-            menu: false,
-            openNote: false,
-
-            
-        }
-    }
-    handleLabel = async(data) => {
-          array.push(data)
-          await this.setState({
-              allLabels:array,
-              labelpresent:true,
-          })
-          console.log(this.state.allLabels)
-
-    }
-    handleLabelRemove = async (data) => {
-
-        array.pop(data)
-        await this.setState({
-          allLabels: array
-        })
-        console.log(this.state.allLabels)
-        if (this.state.allLabels.length === 0) {
-          this.setState({ labelpresent: false })
-        }
-      }
-    MenuClose = () => {
-        this.setState({ menu: false });
-      };
+class CreateNote extends Component {
+  
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
 }
+
+export default CreateNote

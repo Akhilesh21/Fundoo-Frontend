09896eba194e00c766b9a47cf2f56175b10007cb
Akhilesh21 <Akhilesh.mc4@gmail.com>
2020-03-16 09:43:40 +0530

    [Akhilesh]Refactor. added this.state to  title  decription , color to CreateNote.jsx file

--- src/components/CreateNote.jsx
+++ src/components/CreateNote.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
 
+
 class CreateNote extends Component {
   constructor(props){
     super(props);
     this.state= {
-      
-    }
+      cardOpen : false,
+      anchorEl : null,
+      title: "",
+      description: "",
+      color: "",
+      isPinned:false
+    };
   }
   render() {
     return (

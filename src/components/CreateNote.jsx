eb0f492b28359747b771517399a9c687e2d5ac86
Akhilesh21 <Akhilesh.mc4@gmail.com>
2020-03-16 09:40:23 +0530

    [Akhilesh]Refactor. added this.state to  title and decription to  create note class component

--- src/components/CreateNote.jsx
+++ src/components/CreateNote.jsx
@@ -2,7 +2,10 @@ import React, { Component } from 'react'
 
 class CreateNote extends Component {
   constructor(props){
-    
+    super(props);
+    this.state= {
+      
+    }
   }
   render() {
     return (
